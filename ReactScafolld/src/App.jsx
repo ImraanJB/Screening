6723bdcff760e42280d183f769c55b1b17586984
Imraan JB <imranjhabua@gmail.com>
2020-09-06 10:39:07 +0530

    Git Commit for solutions

---
+++ ReactScafolld/src/App.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import "./App.style.less"
+class App extends Component {
+ render() {
+  return(
+   <div>
+    <h1> Production Enviornment {process.env.NODE_ENV}</h1>
+   </div>
+  );
+ }
+}
+export default App
